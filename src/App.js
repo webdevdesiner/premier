e3ddc2755d7dd82de4c2d2969696da40c1da6e14
user <user@users-MacBook-Pro.local>
2024-04-09 19:13:55 -0300

    primeiro commit Premier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fundoVerde'> 
+    <header>header</header>
+    <mains>main</mains>
+    <footer>footer</footer>
+      
     </div>
   );
 }
---
+++ src/Pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css';
+
+const Home = () => {
+    return(
+        <div className = "fundoVerde">
+                <h2>home js</h2>
+        </div>
+    )
+}
+
+export default Home
