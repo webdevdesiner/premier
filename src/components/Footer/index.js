4ace70506dc7479cb2d982ae4060e365e757b0f7
user <user@users-MacBook-Pro.local>
2024-04-16 12:23:19 -0300

    icones fab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,10 @@ import './styles.css'
 import zap from './Ativo 2.png'
 
 
+//<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+//<link rel="stylesheet" href="styles.css">
+
+
 const Footer = () => {
   return (
     <div className='footerArea'>
